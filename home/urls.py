efa0b8ceb65570de9eda423037247031c5b82cf1
Nguyen Thi Cam Tien <93026517+Tienri291@users.noreply.github.com>
2021-11-30 00:40:00 +0700

    add django

---
+++ home/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home),
+    path('review/', views.review),
+    path('postdanang/',views.reviewpost),
+    path('support/',views.support),
+    path('login/',views.login),
+    path('signin/',views.signin),
+    path('tourticket',views.tourticket),
+    path('moveticket',views.moveticket),
+    path('roomticket',views.roomticket),
+    path('otherservicesticket',views.otherservicesticket)
+
+]
---
+++ home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, request, response
+# Create your views here.
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def review(request):
+    return render(request, 'pages/review.html')
+
+def reviewpost(request):
+    return render(request, 'pages/reviewpost.html')
+
+def support(request):
+    return render(request, 'pages/support.html')
+    
+def login(request):
+    return render(request, 'pages/createacc.html')
+
+def signin(request):
+    return render(request, 'pages/signin.html')
+
+def tourticket(request):
+    return render(request, 'pages/tourticket.html')
+
+def moveticket(request):
+    return render(request, 'pages/moveticket.html')
+
+def roomticket(request):
+    return render(request, 'pages/roomticket.html')
+
+def otherservicesticket(request):
+    return render(request, 'pages/otherservicesticket.html')
