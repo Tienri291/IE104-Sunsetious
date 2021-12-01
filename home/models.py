6bc2f70446981a331d61097fd5984869adecaa62
2Dooh <tu.dominh2k@gmail.com>
2021-12-02 02:03:29 +0700

    update register backend
    Co-authored-by: HoagSon <HoagSon@users.noreply.github.com>

--- home/models.py
+++ home/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# class ADMIN(models.Model):
+#     MaAM = models.CharField(max_length=10, primary_key=True)
+#     HoTen = models.CharField(max_length=30)
+#     Email = models.CharField()
+#     Username = models.CharField()
+#     Password = models.CharField()
+
+class KHACHHANG (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    fullname = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=16)
+
+# class DANHMUC (models.Model):
+#     MaDM = models.CharField(max_length=10, primary_key=True)
+#     TenDM = models.CharField()
 
 
+# class SANPHAM (models.Model):
+#     MaSP = models.CharField(max_length=10, primary_key=True)
+#     TenSP = models.CharField(max_length=30)
+#     MaDM = models.ForeignKey(DANHMUC, verbose_name=(""), on_delete=models.CASCADE)
+#     GiaGoc = models.FloatField()
+#     GiaGiam = models.FloatField()
+#     Image_list = models.ImageField()
+#     DanhGia = models.CharField()
+#     LuotXem = models.Count()
+#     Slug = models.SlugField()
+#     Mota = models.CharField()
+
+
+# class DONHANG (models.Model):
+#     MaDH = models.CharField(max_length=10, primary_key=True)
+#     TenDH = models.CharField(max_length=100)
+#     MaSP = models.ForeignKey(SANPHAM, verbose_name=(""), on_delete=models.CASCADE)
+#     MaDM = models.ForeignKey(DANHMUC, verbose_name=_(""), on_delete=models.CASCADE)
+#     NgayBD = models.DateTimeField(auto_now_add=True)
+#     DonGia = models.FloatField()
+#     MaGiamGia = models.CharField(max_length=10)
+
+# class GIOHANG (models.Model):
+#     MaKH = models.CharField(max_length=10, primary_key=True)
+#     MaDH = models.CharField(max_length=10, primary_key=True)
+#     HoTen = models.CharField(max_length=30)
+#     NgayThem = models.DateTimeField(auto_now_add=True)
+#     CapNhatNgayThem = models.DateTimeField(auto_now_add=True)
+#     TongTien = models.FloatField()
+
+# class MAGIAMGIA (models.Model):
+#     MaGG = models.CharField(max_length=15, primary_key=True)
+#     TenMa = models.CharField(max_length=15)
+#     PhanTramGiam = models.IntegerField()
+
+# class CTHD (models.Model):
+#     MaSP = models.CharField(SANPHAM,max_length=10, primary_key=True)
+#     MaDH = models.CharField(DONHANG, max_length=10, primary_key=True)
+
+# class GG_DH (models.Model):
+#     MaGG = models.CharField(MAGIAMGIA,max_length=15, primary_key=True)
+#     MaDH = models.CharField(DONHANG, max_length=10, primary_key=True)
+
+# class GH_SP (models.Model):
+#     MaKH = models.CharField(KHACHHANG,max_length=10, primary_key=True)
+#     MaSP = models.CharField(SANPHAM,max_length=10, primary_key=True)
--- home/urls.py
+++ home/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('', views.home),
     path('review/', views.review),
     path('postdanang/',views.reviewpost),
     path('support/',views.support),
-    path('login/',views.login),
-    path('signin/',views.signin),
+    path('login/',views.register_page),
+    path('signin/',views.register_page),
     path('tourticket',views.tourticket),
     path('moveticket',views.moveticket),
     path('roomticket',views.roomticket),
